02d037e698961bf9004125161d350b3dffe2087d
Nikhil Shah <nikhil.shah.c@revature.com>
2024-10-17 15:24:46 +0530

    Added hello in index.cshtml

---
+++ myDevApp_Ayush/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myDevApp_Ayush.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
